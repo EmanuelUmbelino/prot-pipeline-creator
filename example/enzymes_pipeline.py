68788388e0d4c6601dcecbf6b12ee3217e61c3a7
Emanuel Umbelino <umbelino.emanuel@gmail.com>
2025-12-07 17:30:28 -0300

    fix path

--- example/enzymes_pipeline.py
+++ example/enzymes_pipeline.py
@@ -10,7 +10,7 @@ class EnzymesPipeline():
             print(f"Enzymes Research - Dividing by Enzyme Class")
             PPC_Printer.print_in_file(path + '/enzymes_info.txt', df, EnzymesPipeline.enzymes_info)
             EnzymesPipeline.oxidoreductases_enzymes(df, path + '/1_oxidoreductases.tsv')
-            EnzymesPipeline.transferases_enzymes(df, path + '/2_ transferases.tsv')
+            EnzymesPipeline.transferases_enzymes(df, path + '/2_transferases.tsv')
             EnzymesPipeline.hydrolases_enzymes(df, path + '/3_hydrolases.tsv')
             EnzymesPipeline.lyases_enzymes(df, path + '/4_lyases.tsv')
             EnzymesPipeline.isomerases_enzymes(df, path + '/5_isomerases.tsv')
